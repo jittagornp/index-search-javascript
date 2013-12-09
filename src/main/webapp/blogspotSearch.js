9bbae9286f1a8809cdafbb43c7f3fc75ed67a104
jittagornp <jittagornp@anonymous>
2013-12-10 02:19:59 +0700

    add blogspot search script

--- src/main/webapp/blogspotSearch.js
+++ src/main/webapp/blogspotSearch.js
@@ -1,7 +1,17 @@
+/**
+ * blogspot search plugin
+ * by index search javascript
+ * code : https://github.com/jittagornp/index-search-javascript
+ * 
+ * @author jittagorn pitakmetagoon
+ * create 10/12/2013
+ */
 (function(window, document, controller) {
+
     var resourceJS = [
         'https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/jquery.js',
-        'https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/additionalDictionaries.js',   
+        'https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/moment.js',
+        'https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/additionalDictionaries.js',
         'https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/indexsearch.js'
     ];
 
@@ -10,16 +20,21 @@
     ];
 
     window.onload = function() {
-        var html = ['<input id="searchInput"/><button id="clearButton">clear</button>',
-            '<div id="result">',
-            '<div id="summary"></div>',
-            '<div id="suggestions"></div>',
-            '<div id="repositories"></div>',
+        var na5centScript = document.getElementsByClassName('ns-blogsearch-script')[0];
+        //
+        var blogspotURL = na5centScript.getAttribute('data-blogspot-url');
+        var languages = JSON.parse(na5centScript.getAttribute('data-languages'));
+        var targetElementId = na5centScript.getAttribute('data-element-id');
+
+        var html = ['<input id="na5centSearchInput"/><button id="na5centClearButton">' + languages.CLEAR + '</button>',
+            '<div id="na5centResult">',
+            '<div id="na5centSummary"></div>',
+            '<div id="na5centSuggestions"></div>',
+            '<div id="na5centRepositories"></div>',
             '</div>'
         ];
 
-        var blogElement = getNsScriptAttribute('data-element-id');
-        document.getElementById(blogElement).innerHTML = html.join('');
+        document.getElementById(targetElementId).innerHTML = html.join('');
 
         for (var index in resourceStyle) {
             loadStyle(resourceStyle[index]);
@@ -27,8 +42,10 @@
 
         for (var index in resourceJS) {
             loadScript(resourceJS[index], function() {
-                if (index == 2) {
-                    controller(getNsScriptAttribute('data-blogspot-url'));
+                if (index == 3) {
+                    if (controller) {
+                        controller(blogspotURL, languages, window.jQuery);
+                    }
                 }
             });
         }
@@ -54,12 +71,11 @@
 
         getHeadElement().appendChild(script);
     }
-
-    function getNsScriptAttribute(attributeName) {
-        return document.getElementsByClassName('ns-blogsearch-script')[0].getAttribute(attributeName);
+})(window, document, function(blogspotURL, languages, $) {
+    if (!window.Blogger || !$) {
+        return;
     }
-})(window, document, function(blogspotURL) {
-    
+
     Blogger.pullRepositoryFromBlog(blogspotURL, function(repository) {
         var settings = {
             repository: repository, //require
@@ -72,37 +88,39 @@
         //====================================================================================
 
 
-        var $searchInput = $('#searchInput');
-        var $clearButton = $('#clearButton');
-        var $summary = $('#summary');
-        var $suggestions = $('#suggestions');
-        var $repositories = $('#repositories');
+        var $searchInput = $('#na5centSearchInput');
+        var $clearButton = $('#na5centClearButton');
+        var $summary = $('#na5centSummary');
+        var $suggestions = $('#na5centSuggestions');
+        var $repositories = $('#na5centRepositories');
 
         showResult(repository);
         $searchInput.keyup(function() {
             var keyword = $searchInput.val();
             var result = indexSearch__.search(keyword);
 
-            $summary.text('');
+            $summary.text('').hide();
             if (result.getTotalPosition() !== 0) {
-                $summary.append('search \'')
+                $summary.append(languages.SEARCH + ' \'')
                         .append($('<span>').text(indexSearch__.getKeyword()).addClass('summary-highlight'))
-                        .append('\' found ')
+                        .append('\' ' + languages.FOUND + ' ')
                         .append($('<span>').text(result.getTotalPosition()).addClass('summary-highlight'))
-                        .append(' positions on ')
+                        .append(' ' + languages.POSITIONS + ' ')
                         .append($('<span>').text(result.getTotalSentence()).addClass('summary-highlight'))
-                        .append(' sentances.');
+                        .append(' ' + languages.SENTENCES)
+                        .show();
             } else if (keyword !== '') {
-                $summary.append('search \'')
+                $summary.append(languages.SEARCH + ' \'')
                         .append($('<span>').text(indexSearch__.getKeyword()).addClass('summary-highlight'))
-                        .append('\' not found.');
+                        .append('\' ' + languages.NOT_FOUND)
+                        .show();
             }
 
 
-            $suggestions.text('');
+            $suggestions.text('').hide();
             var suggestions = result.getSuggestions();
             if (suggestions.length !== 0) {
-                $suggestions.append('do you mean ');
+                $suggestions.append(languages.DO_YOU_MEAN + ' ');
                 for (var suggestIndex in suggestions) {
                     var suggest = suggestions[suggestIndex];
                     var highlight = suggest.highlight;
@@ -122,7 +140,7 @@
                     $suggestions.append($suggestItem);
                 }
 
-                $suggestions.append(' ').append('?');
+                $suggestions.append(' ').append('?').show();
             }
 
             showResult(result.getContent());
@@ -159,13 +177,17 @@
                         .append($childDOM);
 
                 if (childNode.published) {
-                    //$list.attr('title', dateFormat(childNode.published));
+                    $list.attr('title', dateFormat(childNode.published));
                 }
 
                 $parentDOM.append($list);
 
                 walkRepositoryShowResult(childNode, $childDOM);
             }
+        }
+
+        function dateFormat(date) {
+            return moment(date).format('MMMM Do YYYY, h:mm:ss a');
         }
     });
 });
