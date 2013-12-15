086e2d0780dda8c16e57a1a9da18c2bfec3867ed
jittagornp <jittagornp@anonymous>
2013-12-15 11:51:11 +0700

    integrate file

--- src/main/webapp/blogspotSearch.js
+++ src/main/webapp/blogspotSearch.js
@@ -293,14 +293,202 @@ var additionalDictionaries="\u0e2b\u0e25\u0e32\u0e22 \u0e14\u0e39\u0e41\u0e25 \u
 
 
 
-(function(b,d,k){function a(a){var f=d.createElement("link");f.setAttribute("rel","stylesheet");f.setAttribute("href",a);d.getElementsByTagName("head")[0].appendChild(f)}function q(a,f){var e=d.createElement("script");e.type="text/javascript";e.onload=f;e.src=a;d.getElementsByTagName("head")[0].appendChild(e)}var l=[],g=["https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/blogspotSearch.css"];b.onload=function(){var c=d.getElementsByClassName("ns-blogsearch-script")[0],
-f=c.getAttribute("data-blogspot-url"),e=c.getAttribute("data-languages")?JSON.parse(c.getAttribute("data-languages")):{},n=c.getAttribute("data-element-id"),h=c.getAttribute("data-additionalDictionaries")||[];"true"==(c.getAttribute("data-slide-search")||!1)&&(l.push("https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/slideSearch.js"),g.push("https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/jscrollpane/jscrollpane.css"),g.push("https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/slideSearch.css"));
-c=['<div class="ns-plugin-search-scroll"><div class="ns-plugin-search-scroll-content"><div class="ns-plugin-search-box" style="display : none;"><input id="na5centSearchInput" placeholder="'+(e.PLACE_HOLDER||"search...")+'"/><button id="na5centClearButton">'+(e.CLEAR||"clear")+"</button>",'<div id="na5centResult">','<div id="na5centSummary"></div>','<div id="na5centSuggestions"></div>','<div id="na5centRepositories"></div>','</div></div></div><div class="ns-plugin-search-button"></div>'];d.getElementById(n).innerHTML=
-c.join("");d.getElementById(n).className+="ns-plugin-slide-search";for(var p in g)a(g[p]);if(0<l.length)for(p in l)q(l[p],function(){p==l.length-1&&k&&k(f,e,h,b.jQuery)});else k&&k(f,e,h,b.jQuery)}})(window,document,function(b,d,k,a){window.Blogger&&a&&Blogger.pullRepositoryFromBlog(b,function(b){function l(d){var e=a("<ul>");p.html(e);g(d,e)}function g(d,e){var f=d.nodes;if(f&&0!==f.length)for(var c in f){var m=f[c],b=a("<a>").attr("href",m.link).html(m.nameHighlight||m.name),l=m.newPost?a("<span>").text("New!").addClass("index-search-new-post"):
-"",h=a("<ol>"),b=a("<li>").attr("class",m.level).append(b).append(l).append(h);m.published&&b.attr("title",moment(m.published).format("MMMM Do YYYY, h:mm:ss a"));e.append(b);g(m,h)}}var c={repository:b,indexOnFields:["name"],maximumIndexKeySize:5,additionalDictionaries:k&&0<k.length?k.split(","):window.additionalDictionaries},f=new IndexSearch(c),e=a("#na5centSearchInput"),c=a("#na5centClearButton"),n=a("#na5centSummary"),h=a("#na5centSuggestions"),p=a("#na5centRepositories");a(".ns-plugin-search-box").show();
-l(b);e.keyup(function(){var b=e.val(),c=f.search(b);n.text("").hide();0!==c.getTotalPosition()?n.append((d.SEARCH||"search")+" '").append(a("<span>").text(f.getKeyword()).addClass("summary-highlight")).append("' "+(d.FOUND||"found")+" ").append(a("<span>").text(c.getTotalPosition()).addClass("summary-highlight")).append(" "+(d.POSITIONS||"positions on")+" ").append(a("<span>").text(c.getTotalSentence()).addClass("summary-highlight")).append(" "+(d.SENTENCES||"sentences.")).show():""!==b&&n.append((d.SEARCH||
-"search")+" '").append(a("<span>").text(f.getKeyword()).addClass("summary-highlight")).append("' "+(d.NOT_FOUND||"not found")).show();h.text("").hide();b=c.getSuggestions();if(0!==b.length){h.append((d.DO_YOU_MEAN||"do you mean")+" ");for(var k in b){var g=b[k],m=g.highlight;0!=k&&h.append(", ");g=a("<a>").attr("href","#"+g.word).attr("data-suggest",g.word).html(m).click(function(b){b.preventDefault();e.val(a(this).attr("data-suggest")).keyup()});h.append(g)}h.append(" ").append("?").show()}l(c.getContent())});
-c.click(function(){e.val("");f.clear();n.text("");h.text("");l(b)})})});
+(function(window, document, controller) {
+    var resourceJS = [
+    ];
+
+    var resourceStyle = [
+        'https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/blogspotSearch.css'
+    ];
+
+    window.onload = function() {
+        //
+        var na5centScript = document.getElementsByClassName('ns-blogsearch-script')[0];
+        var blogspotURL = na5centScript.getAttribute('data-blogspot-url');
+        var languages = na5centScript.getAttribute('data-languages') ? JSON.parse(na5centScript.getAttribute('data-languages')) : {};
+        var targetElementId = na5centScript.getAttribute('data-element-id');
+        var additionalDictionaries = na5centScript.getAttribute('data-additionalDictionaries') || [];
+        var slideSearch = na5centScript.getAttribute('data-slide-search') || false;
+        if (slideSearch == 'true') {
+            resourceJS.push('https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/slideSearch.js');
+
+            resourceStyle.push('https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/jscrollpane/jscrollpane.css');
+            resourceStyle.push('https://rawgithub.com/jittagornp/index-search-javascript/master/src/main/webapp/slideSearch.css');
+        }
+
+        var html = ['<div class="ns-plugin-search-scroll"><div class="ns-plugin-search-scroll-content"><div class="ns-plugin-search-box" style="display : none;"><input id="na5centSearchInput" placeholder="' + (languages.PLACE_HOLDER || 'search...') + '"/><button id="na5centClearButton">' + (languages.CLEAR || 'clear') + '</button>',
+            '<div id="na5centResult">',
+            '<div id="na5centSummary"></div>',
+            '<div id="na5centSuggestions"></div>',
+            '<div id="na5centRepositories"></div>',
+            '</div></div></div><div class="ns-plugin-search-button"></div>'
+        ];
+
+        document.getElementById(targetElementId).innerHTML = html.join('');
+        document.getElementById(targetElementId).className += 'ns-plugin-slide-search';
+
+        for (var index in resourceStyle) {
+            loadStyle(resourceStyle[index]);
+        }
+
+        if (resourceJS.length > 0) {
+            for (var index in resourceJS) {
+                loadScript(resourceJS[index], function() {
+                    if (index == (resourceJS.length - 1) && controller) {
+                        controller(blogspotURL, languages, additionalDictionaries, window.jQuery);
+                    }
+                });
+            }
+        } else {
+            if (controller) {
+                controller(blogspotURL, languages, additionalDictionaries, window.jQuery);
+            }
+        }
+    };
+
+    function getHeadElement() {
+        return document.getElementsByTagName('head')[0];
+    }
+
+    function loadStyle(styleUrl) {
+        var link = document.createElement('link');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', styleUrl);
+
+        getHeadElement().appendChild(link);
+    }
+
+    function loadScript(jsURL, callback) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = callback;
+        script.src = jsURL;
+        //script.async = false;
+
+        getHeadElement().appendChild(script);
+    }
+})(window, document, function(blogspotURL, languages, additionalDictionaries, $) {
+    if (!window.Blogger || !$) {
+        return;
+    }
+
+    Blogger.pullRepositoryFromBlog(blogspotURL, function(repository) {
+        var settings = {
+            repository: repository, //require
+            indexOnFields: ['name'], //require
+            maximumIndexKeySize: 5,
+            additionalDictionaries: (additionalDictionaries && additionalDictionaries.length > 0) ? additionalDictionaries.split(',') : window.additionalDictionaries
+        };
+
+        var indexSearch__ = new IndexSearch(settings);
+        //====================================================================================
+
+
+        var $searchInput = $('#na5centSearchInput');
+        var $clearButton = $('#na5centClearButton');
+        var $summary = $('#na5centSummary');
+        var $suggestions = $('#na5centSuggestions');
+        var $repositories = $('#na5centRepositories');
+        $('.ns-plugin-search-box').show();
+
+        showResult(repository);
+        $searchInput.keyup(function() {
+            var keyword = $searchInput.val();
+            var result = indexSearch__.search(keyword);
+
+            $summary.text('').hide();
+            if (result.getTotalPosition() !== 0) {
+                $summary.append((languages.SEARCH || 'search') + ' \'')
+                        .append($('<span>').text(indexSearch__.getKeyword()).addClass('summary-highlight'))
+                        .append('\' ' + (languages.FOUND || 'found') + ' ')
+                        .append($('<span>').text(result.getTotalPosition()).addClass('summary-highlight'))
+                        .append(' ' + (languages.POSITIONS || 'positions on') + ' ')
+                        .append($('<span>').text(result.getTotalSentence()).addClass('summary-highlight'))
+                        .append(' ' + (languages.SENTENCES || 'sentences.'))
+                        .show();
+            } else if (keyword !== '') {
+                $summary.append((languages.SEARCH || 'search') + ' \'')
+                        .append($('<span>').text(indexSearch__.getKeyword()).addClass('summary-highlight'))
+                        .append('\' ' + (languages.NOT_FOUND || 'not found'))
+                        .show();
+            }
+
+
+            $suggestions.text('').hide();
+            var suggestions = result.getSuggestions();
+            if (suggestions.length !== 0) {
+                $suggestions.append((languages.DO_YOU_MEAN || 'do you mean') + ' ');
+                for (var suggestIndex in suggestions) {
+                    var suggest = suggestions[suggestIndex];
+                    var highlight = suggest.highlight;
+
+                    if (suggestIndex != 0) {
+                        $suggestions.append(', ');
+                    }
+
+                    var $suggestItem = $('<a>').attr('href', '#' + suggest.word)
+                            .attr('data-suggest', suggest.word)
+                            .html(highlight)
+                            .click(function(event) {
+                        event.preventDefault();
+                        $searchInput.val($(this).attr('data-suggest')).keyup();
+                    });
+
+                    $suggestions.append($suggestItem);
+                }
+
+                $suggestions.append(' ').append('?').show();
+            }
+
+            showResult(result.getContent());
+        });
+
+        $clearButton.click(function() {
+            $searchInput.val('');
+            indexSearch__.clear();
+            $summary.text('');
+            $suggestions.text('');
+            showResult(repository);
+        });
+
+        function showResult(rootNode) {
+            var $rootDOM = $('<ul>');
+            $repositories.html($rootDOM);
+            walkRepositoryShowResult(rootNode, $rootDOM);
+        }
+
+        function walkRepositoryShowResult(parentNode, $parentDOM) {
+            var nodes = parentNode.nodes;
+            if (!nodes || nodes.length === 0) {
+                return;
+            }
+
+            for (var index in nodes) {
+                var childNode = nodes[index];
+                var $childLink = $('<a>').attr('href', childNode.link).html(childNode.nameHighlight || childNode.name);
+                var $newPost = childNode.newPost ? $('<span>').text('New!').addClass('index-search-new-post') : '';
+                var $childDOM = $('<ol>');
+                var $list = $('<li>').attr('class', childNode.level)
+                        .append($childLink)
+                        .append($newPost)
+                        .append($childDOM);
+
+                if (childNode.published) {
+                    $list.attr('title', dateFormat(childNode.published));
+                }
+
+                $parentDOM.append($list);
+
+                walkRepositoryShowResult(childNode, $childDOM);
+            }
+        }
+
+        function dateFormat(date) {
+            return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+        }
+    });
+});
+
 
 
 
