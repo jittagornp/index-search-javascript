a8df60029f20f753d8375f3fe4a45294a30bd269
jittagornp <jittagornp@anonymous>
2013-12-15 17:45:40 +0700

    add facebook theme

--- src/main/webapp/slideSearch.js
+++ src/main/webapp/slideSearch.js
@@ -1,47 +1,48 @@
 (function($) {
 
-//    $('.ns-plugin-search-scroll').jScrollPane({
-//        showArrows: false,
-//        maintainPosition: true,
-//        stickToBottom: false,
-//        stickToRight: false,
-//        clickOnTrack: true,
-//        autoReinitialise: true,
-//        autoReinitialiseDelay: 0,
-//        verticalDragMinHeight: 0,
-//        verticalDragMaxHeight: 99999,
-//        horizontalDragMinWidth: 0,
-//        horizontalDragMaxWidth: 99999,
-//        animateScroll: true,
-//        animateDuration: 500,
-//        animateEase: 'linear',
-//        hijackInternalLinks: false,
-//        verticalGutter: 0,
-//        horizontalGutter: 0,
-//        mouseWheelSpeed: 0,
-//        arrowButtonSpeed: 0,
-//        arrowRepeatFreq: 50,
-//        arrowScrollOnHover: false,
-//        trackClickSpeed: 0,
-//        trackClickRepeatFreq: 70,
-//        verticalArrowPositions: 'split',
-//        horizontalArrowPositions: 'split',
-//        enableKeyboardNavigation: true,
-//        hideFocus: false,
-//        keyboardSpeed: 0,
-//        initialDelay: 0,
-//        speed: 50,
-//        scrollPagePercent: 0.8
-//    }).on('mousewheel', function(event) {
-//        event.preventDefault();
-//    });
-//
-//    $(window).on('resize', function() {
-//        var jsp = $('.ns-plugin-search-scroll').data('jsp');
-//        if (jsp) {
-//            jsp.reinitialise();
-//        }
-//    });
+    $('.ns-plugin-search-scroll').jScrollPane({
+        showArrows: false,
+        maintainPosition: true,
+        stickToBottom: false,
+        stickToRight: false,
+        clickOnTrack: true,
+        autoReinitialise: true,
+        autoReinitialiseDelay: 500,
+        verticalDragMinHeight: 80,
+        verticalDragMaxHeight: 99999,
+        horizontalDragMinWidth: 0,
+        horizontalDragMaxWidth: 99999,
+        contentWidth: undefined,
+        animateScroll: false,
+        animateDuration: 300,
+        animateEase: 'linear',
+        hijackInternalLinks: false,
+        verticalGutter: 5,
+        horizontalGutter: 2,
+        mouseWheelSpeed: 0,
+        arrowButtonSpeed: 0,
+        arrowRepeatFreq: 50,
+        arrowScrollOnHover: false,
+        trackClickSpeed: 0,
+        trackClickRepeatFreq: 70,
+        verticalArrowPositions: 'split',
+        horizontalArrowPositions: 'split',
+        enableKeyboardNavigation: true,
+        hideFocus: false,
+        keyboardSpeed: 0,
+        initialDelay: 300, // Delay before starting repeating
+        speed: 30, // Default speed when others falsey
+        scrollPagePercent: .8		// Percent of visible area scrolled when pageUp/Down or track area pressed
+    }).bind('mousewheel', function(event) {
+        event.preventDefault();
+    });
+
+    $(window).on('resize', function() {
+        var jsp = $('.ns-plugin-search-scroll').data('jsp');
+        if (jsp) {
+            jsp.reinitialise();
+        }
+    });
 
 
     $(function() {
