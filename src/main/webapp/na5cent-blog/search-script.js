c4e7e8f838316be266f0a87647b92afee16f3759
jittagornp <jittagornp@anonymous>
2014-01-17 11:12:22 +0700

    lazy make an index (first time only when click expand button)

--- src/main/webapp/na5cent-blog/search-script.js
+++ src/main/webapp/na5cent-blog/search-script.js
@@ -366,7 +366,7 @@ k.clear();l.text("");d.text("");n(m)})})})(jQuery,repository);
                     window.clearTimeout(timeout);
                     makeIndex();
                     indexes = true;
-                }, 50);
+                }, speedAnimation);
             }
         } else {
             $leftSlider.removeClass("expand");
